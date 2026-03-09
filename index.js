686e8fc8eb45e9ae721f178b8f91c95788c8620b
fbigx197 <belen.alexa121@gmail.com>
2026-03-09 01:52:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.once("ready", () => {
+client.once('ready', () => {
   console.log(`Bot conectado como ${client.user.tag}`);
 });
 
-client.on("messageCreate", message => {
-  if(message.content === "!ping"){
-    message.reply("pong 🏓");
+client.on('messageCreate', message => {
+  if (message.content === "!ping") {
+    message.reply("Pong 🏓");
   }
 });
 
