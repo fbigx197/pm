ad3eed00da18df34eb456b9348a5f31a9f1a5b05
fbigx197 <belen.alexa121@gmail.com>
2026-03-09 01:33:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.log("Bot iniciado correctamente");
+const { Client, GatewayIntentBits } = require("discord.js");
 
-setInterval(() => {
-  console.log("El bot sigue activo...");
-}, 5000);
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once("ready", () => {
+  console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+client.on("messageCreate", message => {
+  if(message.content === "!ping"){
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
